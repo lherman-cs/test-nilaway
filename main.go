bdae712cf0a736fe089c3baf361bea3a479a9cf8
Lukas Herman <lherman.cs@gmail.com>
2024-05-15 15:29:50 -0400

    update

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func foo() *int {
+	return nil
+}
+
+func bar() {
+	print(*foo()) // nilness reports NO error here, but NilAway does.
+}
+
 func hello() *int {
 	return nil
 }
